654b43e4960de7a89ce696ab5f29a0576d262939
clicktx <clicktx@gmail.com>
2016-06-06 09:48:44 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MojoX::Session::Store::Teng - It's new $module
+
+# SYNOPSIS
+
+    use MojoX::Session::Store::Teng;
+
+# DESCRIPTION
+
+MojoX::Session::Store::Teng is ...
+
+# LICENSE
+
+Copyright (C) clicktx.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+clicktx <clicktx@gmail.com>
---
+++ lib/MojoX/Session/Store/Teng.pm
@@ -0,0 +1,39 @@
+package MojoX::Session::Store::Teng;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MojoX::Session::Store::Teng - It's new $module
+
+=head1 SYNOPSIS
+
+    use MojoX::Session::Store::Teng;
+
+=head1 DESCRIPTION
+
+MojoX::Session::Store::Teng is ...
+
+=head1 LICENSE
+
+Copyright (C) clicktx.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+clicktx E<lt>clicktx@gmail.comE<gt>
+
+=cut
+
